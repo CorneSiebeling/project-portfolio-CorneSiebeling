0204e343d7f42de86a202d1af5362814977b9978
Corné Siebeling <2078538@talnet.nl>
2019-11-06 11:32:59 +0100

    contact gefixt

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function makeOrange(){
+    document.getElementById("dislike").style.backgroundColor = "red" ;
+    document.getElementById("like").style.backgroundColor = "black" ;
+    
+}
+
+function makeGrape(){
+    document.getElementById("like").style.backgroundColor = "green" ;
+    document.getElementById("dislike").style.backgroundColor = "black" ;
+}
