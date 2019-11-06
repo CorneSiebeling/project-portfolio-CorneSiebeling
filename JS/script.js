c9773be84523220c6212933d30f98819e0498efb
Corné Siebeling <2078538@talnet.nl>
2019-11-06 11:50:45 +0100

    like/dislike button

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,3 @@
-function makeOrange(){
-    document.getElementById("dislike").style.backgroundColor = "red" ;
-    document.getElementById("like").style.backgroundColor = "black" ;
-    
-}
-
-function makeGrape(){
-    document.getElementById("like").style.backgroundColor = "green" ;
-    document.getElementById("dislike").style.backgroundColor = "black" ;
-}
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+  }
